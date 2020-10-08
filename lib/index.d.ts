ecb9966a66649e39fa33d4bb5bd1df7756be4559
douglas oliveira <douglasolv.dev@gmail.com>
2020-10-08 16:35:48 -0300

    .gitignore update

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { TextInputProps } from "react-native";
-interface InputProps extends TextInputProps {
-    fontColor: string;
-    label: string;
-}
-export declare const TextinputAnimated: React.FunctionComponent<InputProps>;
-export {};
-//# sourceMappingURL=index.d.ts.map
